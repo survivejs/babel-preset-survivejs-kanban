5e2f76b82de870f44bf6df30bec9066a59c17001
Juho Vepsalainen <bebraw@gmail.com>
2016-01-27 16:12:02 +0200

    Drop `default` bits
    Looks like these aren't needed anymore. Very strange (Babel bug?).

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 module.exports = {
   plugins: [
-    require('babel-plugin-array-includes').default,
+    require('babel-plugin-array-includes'),
 
     require('babel-plugin-syntax-class-properties'),
     require('babel-plugin-syntax-decorators'),
     require('babel-plugin-syntax-object-rest-spread'),
 
     require('babel-plugin-transform-class-properties'),
-    require('babel-plugin-transform-decorators-legacy').default,
+    require('babel-plugin-transform-decorators-legacy'),
     require('babel-plugin-transform-object-assign'),
     require('babel-plugin-transform-object-rest-spread')
   ]
